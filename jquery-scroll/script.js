00e01ad497b23429dc2233302f3a7d7e2bc4699a
Alex Link <deimoslink@gmail.com>
2016-08-06 18:04:25 +0400

    scroll

---
+++ jquery-scroll/script.js
@@ -0,0 +1,71 @@
+var pictures = [];
+var i = 1;
+
+while (i <= 6) {
+	pictures.push({url:"pics/"+i+".jpg"});
+	i++;
+}
+
+$(document).ready(function(){
+
+	var totalPics = pictures.length;
+
+	(function addPics () {
+		var i = 0;
+		while (i < totalPics) {
+			var element = '<div class="slide"><img src="'+pictures[i].url+'" alt=""></div>';
+			$('.slider-inner').append(element)
+			i++;
+		}
+	}());
+
+	(function createNav () {
+		var listItem = '<li style="margin: 0 2px"></li>';
+		var step = 1;
+		while (step <= totalPics) {
+			$('.slider-nav-list').append(listItem);
+			step++;
+		}
+	}());
+
+	function render () {
+		$(".slide, .slider-nav-list li").removeClass("active");
+		$(".slide:eq("+currentIndex+"), .slider-nav-list li:eq("+currentIndex+")").addClass("active");
+	};
+
+	function nextSlide (){
+		currentIndex ++;
+		if (currentIndex > totalPics - 1) {
+			currentIndex = 0;
+		}
+		render();
+	};
+
+	function prevSlide (){
+		currentIndex --;
+		if (currentIndex < 0) {
+			currentIndex = totalPics - 1;
+		}
+		render();
+	};
+
+	var currentIndex = 0;
+
+	$(".slide:first, .slider-nav-list li:first").addClass("active");
+	$("#sliderNext").on('click',nextSlide);
+	$("#sliderPrev").on('click',prevSlide);
+
+	$(".slider-nav-list li").on('click',function(){
+		currentIndex = $(".slider-nav-list li").index($(this));
+		render();
+	});
+
+	var autoScroll = setInterval(nextSlide, 1000);	
+	
+
+	$('#slider').on('mouseenter',function() {
+		console.log("test");
+		clearInterval(autoScroll);
+	});
+
+});
